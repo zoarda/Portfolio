690b2eef7d01894a5e39dd28ef00b7194078f9a8
shrimp <a0920460509@gmail.com>
2026-07-28 00:22:11 +0800

    novel system

--- Assets/Scripts/UI/DialogueUI.cs
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -6,12 +6,15 @@ public class DialogueUI : MonoBehaviour
     public Text speakerText;
     public Text contentText;
     public Button nextButton;
+    public TypewriterEffect typewriter;
 
     private StoryNode currentNode;
 
     private void Start()
     {
         nextButton.onClick.AddListener(OnNext);
+
+        typewriter.OnTypingComplete += OnTypingComplete;
     }
 
     public void Show(StoryNode node)
@@ -21,7 +24,8 @@ public class DialogueUI : MonoBehaviour
         currentNode = node;
 
         speakerText.text = node.speaker;
-        contentText.text = node.content;
+
+        typewriter.Play(node.content);
     }
 
     public void Hide()
@@ -31,9 +35,43 @@ public class DialogueUI : MonoBehaviour
 
     private void OnNext()
     {
+        Debug.Log($"typewriter = {typewriter}");
+        Debug.Log($"currentNode = {currentNode}");
+        Debug.Log($"StoryManager = {StoryManager.Instance}");
+
+        if (typewriter == null)
+        {
+            Debug.LogError("typewriter is NULL");
+            return;
+        }
+
+        if (currentNode == null)
+        {
+            Debug.LogError("currentNode is NULL");
+            return;
+        }
+
+        if (StoryManager.Instance == null)
+        {
+            Debug.LogError("StoryManager.Instance is NULL");
+            return;
+        }
+
+        Debug.Log($"nextId = {currentNode.nextId}");
+
+        if (typewriter.IsTyping)
+        {
+            typewriter.Skip();
+            return;
+        }
+
         if (currentNode.nextId > 0)
         {
             StoryManager.Instance.GoTo(currentNode.nextId);
         }
+    }
+    private void OnTypingComplete()
+    {
+        Debug.Log("Typing Finish");
     }
 }
--- Assets/Scripts/UI/TypewriterEffect.cs
+++ Assets/Scripts/UI/TypewriterEffect.cs
@@ -1,16 +1,56 @@
+using System;
+using System.Collections;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class TypewriterEffect : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Text targetText;
+    [SerializeField] private float interval = 0.03f;
+
+    private Coroutine typingCoroutine;
+    private string fullText;
+
+    public bool IsTyping { get; private set; }
+
+    public Action OnTypingComplete;
+
+    public void Play(string content)
     {
-        
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+        fullText = content;
+        typingCoroutine = StartCoroutine(TypeRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TypeRoutine()
     {
-        
+        IsTyping = true;
+
+        targetText.text = "";
+
+        foreach (char c in fullText)
+        {
+            targetText.text += c;
+            yield return new WaitForSeconds(interval);
+        }
+
+        IsTyping = false;
+
+        OnTypingComplete?.Invoke();
+    }
+
+    public void Skip()
+    {
+        if (!IsTyping)
+            return;
+
+        StopCoroutine(typingCoroutine);
+
+        targetText.text = fullText;
+
+        IsTyping = false;
+
+        OnTypingComplete?.Invoke();
     }
 }
